ccb0576bc0ca1f184d07a39739ec8d66091a4302
Amankumar0152 <amankumarmaurya93@gmail.com>
2024-06-07 11:35:29 +0530

    latest updates

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,19 +1,7 @@
-document.getElementById('close-button').addEventListener('click', function() {
-    document.getElementById('message-overlay').style.display = 'none';
-    document.cookie = "seenMessage=true; max-age=" + 60*60*24*365 + "; path=/";
-});
-
-function getCookie(name) {
-    let cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if (name === cookiePair[0].trim()) {
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    return null;
+function closePopup() {
+    document.getElementById('popup-overlay').style.display = 'none';
 }
 
-if (getCookie("seenMessage")) {
-    document.getElementById('message-overlay').style.display = 'none';
+window.onload = function() {
+    document.getElementById('popup-overlay').style.display = 'flex';
 }
