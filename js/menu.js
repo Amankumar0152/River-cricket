1320bc4637db8d34248816b928f5553662b0aa66
Amankumar0152 <amankumarmaurya93@gmail.com>
2024-06-30 18:41:56 +0530

    update

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuBtn = document.getElementById('menu-btn');
-    const navbar = document.getElementById('navbar');
-
-    menuBtn.addEventListener('click', function() {
-        navbar.classList.toggle('show');
-        toggleMenuIcon();
+document.addEventListener('DOMContentLoaded', function () {
+    var toggler = document.getElementById('navbar-toggler');
+    var menu = document.getElementById('navbar-menu');
+  
+    toggler.addEventListener('click', function () {
+        menu.classList.toggle('show');
     });
+  });
+  
 
-    // Show the menu button only on mobile devices
-    if (window.innerWidth <= 768) {
-        menuBtn.style.display = 'block';
-    }
-});
 
-function toggleMenuIcon() {
-    const menuBtn = document.getElementById('menu-btn');
-    const spans = menuBtn.querySelectorAll('span');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuBtn = document.getElementById('menu-btn');
+//     const navbar = document.getElementById('navbar');
 
-    spans.forEach(span => {
-        span.classList.toggle('open');
-    });
-}
+//     menuBtn.addEventListener('click', function() {
+//         navbar.classList.toggle('show');
+//         toggleMenuIcon();
+//     });
+
+//     // Show the menu button only on mobile devices
+//     if (window.innerWidth <= 768) {
+//         menuBtn.style.display = 'block';
+//     }
+// });
+
+// function toggleMenuIcon() {
+//     const menuBtn = document.getElementById('menu-btn');
+//     const spans = menuBtn.querySelectorAll('span');
+
+//     spans.forEach(span => {
+//         span.classList.toggle('open');
+//     });
+// }
--- js/newmenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var toggler = document.getElementById('navbar-toggler');
-    var menu = document.getElementById('navbar-menu');
-  
-    toggler.addEventListener('click', function () {
-        menu.classList.toggle('show');
-    });
-  });
-  
